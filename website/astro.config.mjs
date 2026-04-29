4e6237b600be60cc88751f818f218572611e2de4
amakhov <amakhov@mirantis.com>
2026-04-29 23:38:03 +0300

    Posthog instead of GA

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -28,7 +28,8 @@ function rehypePrependBase(base) {
 // In local dev both are unset and the defaults below are used.
 const site = process.env.SITE ?? 'https://t4db.github.io';
 const base = process.env.BASE_PATH ?? '/t4';
-const googleAnalyticsId = process.env.PUBLIC_GA_MEASUREMENT_ID;
+const posthogKey = process.env.PUBLIC_POSTHOG_KEY;
+const posthogHost = process.env.PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com';
 
 const head = [
 	{
@@ -40,36 +41,14 @@ const head = [
 	},
 ];
 
-if (googleAnalyticsId) {
-	head.push(
-		{
-			tag: 'script',
-			content: [
-				'window.dataLayer = window.dataLayer || [];',
-				'function gtag(){dataLayer.push(arguments);}',
-				"gtag('consent', 'default', {",
-				"  ad_storage: 'denied',",
-				"  ad_user_data: 'denied',",
-				"  ad_personalization: 'denied',",
-				"  analytics_storage: 'denied',",
-				'});',
-			].join('\n'),
-		},
-		{
-			tag: 'script',
-			attrs: {
-				async: true,
-				src: `https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`,
-			},
-		},
-		{
-			tag: 'script',
-			content: [
-				"gtag('js', new Date());",
-				`gtag('config', '${googleAnalyticsId}');`,
-			].join('\n'),
-		}
-	);
+if (posthogKey) {
+	head.push({
+		tag: 'script',
+		content: [
+			'!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);',
+			`posthog.init(${JSON.stringify(posthogKey)}, { api_host: ${JSON.stringify(posthogHost)}, defaults: '2026-01-30', persistence: 'memory' });`,
+		].join('\n'),
+	});
 }
 
 // https://astro.build/config
